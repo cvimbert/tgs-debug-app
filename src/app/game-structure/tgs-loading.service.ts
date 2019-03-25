13d2823cce3ab2a61e3c1fb87e91a91fc8346c1a
Christophe <ch.vimbert@gmail.com>
2019-03-25 09:26:09 +0100

    Fix npm link library path bug and add load file method

--- src/app/game-structure/tgs-loading.service.ts
+++ src/app/game-structure/tgs-loading.service.ts
@@ -1,36 +1,17 @@
 import { Injectable } from '@angular/core';
-import { TGSParser, ParsingResult } from 'tgs-parser';
-import { MainStructure } from 'tgs-model';
-import { GameSequence } from 'tgs-core';
+import { GameManager } from 'tgs-core';
 import { TgsMainService } from './tgs-main.service';
-import { GameContext } from 'tgs-core/core/game-context.class';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TgsLoadingService {
+export class TgsLoadingService extends GameManager {
 
   constructor(
     private tgsService: TgsMainService
   ) {
-    GameContext.init();
-  }
-
-  loadFile(path: string): Promise<GameSequence> {
-
-    return new Promise<GameSequence>((success: Function) => {
-
-      let parser: TGSParser = new TGSParser();
-
-      parser.loadTGSFile("assets/tgs/" + path + ".tgs").then((resp: ParsingResult) => {
-        let structure: MainStructure = MainStructure.loadFromParsingResult(resp);
-        let sequence = new GameSequence(structure);
-        sequence.init(path);
-        this.tgsService.currentSequence = sequence;
-        success(sequence);
-      });
+    super({
+      assetsFolder: "assets/"
     });
-
-
   }
 }
